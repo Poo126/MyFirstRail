fd0632e565929894547762c4ffbad9f63828ab35
SK <suveetha@amazon.com>
2013-06-10 19:57:22 +0530

    MyFirstRail Git Commit:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFirstRail::Application.config.secret_token = '62f4f92e52b4182d358f7ca5617c52ff763ea5d28f79ee300abf93e1cf6371e38fc928f1d678f82d8533bcebd4c4fbc65a190a95ed4448435bac3b1e26a4b448'
